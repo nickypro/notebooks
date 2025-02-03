c924d303b93645b0f0f4167d90b10e7138f77dfc
Nicky Pochinkov <work@nicky.pro>
2025-02-03 20:12:23 +0000

    add contexts

--- sonar/load_model.py
+++ sonar/load_model.py
@@ -5,12 +5,16 @@ import torch
 from utils_load_data import load_embeds, load_res_data
 from utils_train import Trainer  # Assumption: You have a Trainer class with loading functionality
 
+NUM_COPIES = 1
+#PREFIX = "sonar-sweeps"
+PREFIX = "notebooks-sonar"
+
 def main():
     parser = ArgumentParser(description='Test the model')
     parser.add_argument('wandb_run_name', type=str,
                        help='Name of the W&B run to load (e.g., northern-sweep-37)')
     args = parser.parse_args()
-    model = Trainer.load_from_wandb("sonar-sweeps/"+args.wandb_run_name)
+    model = Trainer.load_from_wandb(PREFIX+"/"+args.wandb_run_name)
     return model
 
 if __name__ == "__main__":
@@ -27,33 +31,31 @@ if __name__ == "__main__":
         embeds = load_embeds(99)
         res_data = load_res_data(99, groups_to_load=trainer.c.groups_to_load)
         for index in [1, 42, 100, 200, 300, 500, 800, 1000, 1234, 2345, 3456]:
+            prev_emb = embeds[index-1].unsqueeze(dim=0).to(DEVICE)
             orig_emb   = embeds[index].unsqueeze(dim=0).to(DEVICE)
             test_input = trainer.normalizer_res(res_data[index].unsqueeze(dim=0).to(DEVICE))
-            test_input_batch = test_input.repeat(3, 1)  # Create a batch of 3 copies along dim=0
+            test_input_batch = test_input.repeat(NUM_COPIES, 1)  # Create a batch of 3 copies along dim=0
             predicted_embeds = trainer.normalizer_emb.restore(trainer.model(test_input_batch))
             decoded_text = vec2text_model.predict(
-                torch.cat([orig_emb, predicted_embeds], dim=0),
+                torch.cat([prev_emb, orig_emb, predicted_embeds], dim=0),
                 target_lang="eng_Latn"
             )
-            cossim = torch.nn.functional.cosine_similarity(orig_emb[0], predicted_embeds[0], dim=0).item():.4f}
-            print(f"### EXAMPLE {index} ### (SIM = {cossim})")
+            cossim = torch.nn.functional.cosine_similarity(orig_emb[0], predicted_embeds[0], dim=0).item()
+            print(f"### EXAMPLE {index} ### (SIM = {cossim:.4f})")
 
-            print(textwrap.fill(colored(f"ORIGINAL: {decoded_text[0][:200]}", 'blue'),
+            print(textwrap.fill(colored(f"PREV    : {decoded_text[0][:200]}", 'blue'),
                               width=120,
                               initial_indent='',
                               subsequent_indent=' ' * 10))
-            print(textwrap.fill(colored(f"PROBE   : {decoded_text[1][:200]}", 'green'),
+            print(textwrap.fill(colored(f"ORIGINAL: {decoded_text[1][:200]}", 'blue'),
                               width=120,
                               initial_indent='',
                               subsequent_indent=' ' * 10))
-            print(textwrap.fill(colored(f"PROBE   : {decoded_text[2][:200]}", 'green'),
-                              width=120,
-                              initial_indent='',
-                              subsequent_indent=' ' * 10))
-            print(textwrap.fill(colored(f"PROBE   : {decoded_text[3][:200]}", 'green'),
-                              width=120,
-                              initial_indent='',
-                              subsequent_indent=' ' * 10))
+            for i in range(2, len(decoded_text)):
+                print(textwrap.fill(colored(f"PROBE   : {decoded_text[i][:200]}", 'green'),
+                                    width=120,
+                                    initial_indent='',
+                                    subsequent_indent=' ' * 10))
             print()
 
 # %%
